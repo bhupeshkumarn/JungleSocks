0f0d3f6cc9a66caefd9bf0756365b23e23b265f6
bhupeshkumarn <bhupeshkumarn@hotmail.com>
2016-06-12 17:59:22 -0700

    Update JungleSocks.java
    - Added more tests
    - Made final changes to JungleSocks script

--- src/JungleSocks.java
+++ src/JungleSocks.java
@@ -20,7 +20,7 @@ public class JungleSocks {
 
   
   @Test
-  public void testJungleSocks() throws Exception {
+  public void testJungleSocksCA() throws Exception {
 	    driver.get(baseUrl + "/");
 	    driver.findElement(By.id("line_item_quantity_zebra")).clear();
 	    driver.findElement(By.id("line_item_quantity_zebra")).sendKeys("2");
@@ -38,6 +38,26 @@ public class JungleSocks {
 	    } catch (Error e) {
 	      verificationErrors.append(e.toString());
 	    }
+	    try {
+	    	assertEquals("elephant 35 1", driver.findElement(By.className("elephant")).getText());
+		    } catch (Error e) {
+		      verificationErrors.append(e.toString());
+		    }
+	    try {
+	    	assertEquals("lion 20 3", driver.findElement(By.className("lion")).getText());
+		    } catch (Error e) {
+		      verificationErrors.append(e.toString());
+		    }
+	    try {
+	    	assertEquals("zebra 13 2", driver.findElement(By.className("zebra")).getText());
+		    } catch (Error e) {
+		      verificationErrors.append(e.toString());
+		    }
+	    try {
+	    	assertEquals("giraffe 17 1", driver.findElement(By.className("giraffe")).getText());
+		    } catch (Error e) {
+		      verificationErrors.append(e.toString());
+		    }
 	    try {
 	      assertEquals("$138.00", driver.findElement(By.id("subtotal")).getText());
 	    } catch (Error e) {
@@ -58,7 +78,7 @@ public class JungleSocks {
 	  }
   
   @Test
-  public void testJungleSocks2() throws Exception {
+  public void testJungleSocksCoNeg() throws Exception {
     driver.get(baseUrl + "/");
     driver.findElement(By.id("line_item_quantity_zebra")).clear();
     driver.findElement(By.id("line_item_quantity_zebra")).sendKeys("1");
@@ -68,7 +88,7 @@ public class JungleSocks {
     driver.findElement(By.id("line_item_quantity_elephant")).sendKeys("-1");
     driver.findElement(By.id("line_item_quantity_giraffe")).clear();
     driver.findElement(By.id("line_item_quantity_giraffe")).sendKeys("1");
-    //Selecting state of California
+    //Selecting state of Colorado
     new Select(driver.findElement(By.name("state"))).selectByVisibleText("Colorado");
     driver.findElement(By.name("commit")).click();
     try {
@@ -96,7 +116,7 @@ public class JungleSocks {
   }
   
   @Test
-  public void testJungleSocks3() throws Exception {
+  public void testJungleSocksNoItems() throws Exception {
     driver.get(baseUrl + "/");
     driver.findElement(By.id("line_item_quantity_zebra")).clear();
     driver.findElement(By.id("line_item_quantity_lion")).clear();
@@ -111,11 +131,130 @@ public class JungleSocks {
       verificationErrors.append(e.toString());
     }
     try {
-      assertFalse(driver.getTitle().equalsIgnoreCase("We're sorry, but something went wrong (500)"));
+     assertFalse(driver.getTitle().equalsIgnoreCase("We're sorry, but something went wrong (500)"));
     } catch (Error e) {
       verificationErrors.append(e.toString());
     }     
   }
+  @Test
+  public void testJungleSocksMnNoTax() throws Exception {
+    driver.get(baseUrl + "/");
+    driver.findElement(By.id("line_item_quantity_zebra")).clear();
+    driver.findElement(By.id("line_item_quantity_zebra")).sendKeys("1");
+    driver.findElement(By.id("line_item_quantity_lion")).clear();
+    driver.findElement(By.id("line_item_quantity_lion")).sendKeys("2");
+    driver.findElement(By.id("line_item_quantity_elephant")).clear();
+    driver.findElement(By.id("line_item_quantity_elephant")).sendKeys("3");
+    driver.findElement(By.id("line_item_quantity_giraffe")).clear();
+    driver.findElement(By.id("line_item_quantity_giraffe")).sendKeys("4");
+    //Selecting state of Minnesota
+    new Select(driver.findElement(By.name("state"))).selectByVisibleText("Minnesota");
+    driver.findElement(By.name("commit")).click();
+    try {
+      assertFalse(isAlertPresent());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      assertEquals("$226.00", driver.findElement(By.id("subtotal")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+    	/* Minnesota Tax @ 0.00% */
+      assertEquals("$0.00", driver.findElement(By.id("taxes")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+    try {
+      assertEquals("$226.00", driver.findElement(By.id("total")).getText());
+    } catch (Error e) {
+      verificationErrors.append(e.toString());
+    }
+   
+  }
+  
+  @Test
+  public void testJungleSocksNY() throws Exception {
+	    driver.get(baseUrl + "/");
+	    driver.findElement(By.id("line_item_quantity_zebra")).clear();
+	    driver.findElement(By.id("line_item_quantity_zebra")).sendKeys("2");
+	    driver.findElement(By.id("line_item_quantity_lion")).clear();
+	    driver.findElement(By.id("line_item_quantity_lion")).sendKeys("3");
+	    driver.findElement(By.id("line_item_quantity_elephant")).clear();
+	    driver.findElement(By.id("line_item_quantity_elephant")).sendKeys("1");
+	    driver.findElement(By.id("line_item_quantity_giraffe")).clear();
+	    driver.findElement(By.id("line_item_quantity_giraffe")).sendKeys("1");
+	    //Selecting state of NY
+	    new Select(driver.findElement(By.name("state"))).selectByVisibleText("New York");
+	    driver.findElement(By.name("commit")).click();
+	    try {
+	      assertFalse(isAlertPresent());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	      assertEquals("$138.00", driver.findElement(By.id("subtotal")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	    	/* NewYork Tax @ 6.00% */
+	      assertEquals("$8.28", driver.findElement(By.id("taxes")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	      assertEquals("$146.28", driver.findElement(By.id("total")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	   
+	  }
+  
+  @Test
+  public void testJungleSocksExceedsQty() throws Exception {
+	    driver.get(baseUrl + "/");
+	    driver.findElement(By.id("line_item_quantity_zebra")).clear();
+	    driver.findElement(By.id("line_item_quantity_zebra")).sendKeys("0");
+	    driver.findElement(By.id("line_item_quantity_lion")).clear();
+	    driver.findElement(By.id("line_item_quantity_lion")).sendKeys("0");
+	    driver.findElement(By.id("line_item_quantity_elephant")).clear();
+	    driver.findElement(By.id("line_item_quantity_elephant")).sendKeys("5");
+	    driver.findElement(By.id("line_item_quantity_giraffe")).clear();
+	    driver.findElement(By.id("line_item_quantity_giraffe")).sendKeys("0");
+	    //Selecting state of Nebraska
+	    new Select(driver.findElement(By.name("state"))).selectByVisibleText("Nebraska");
+	    driver.findElement(By.name("commit")).click();
+	    try {
+	      assertFalse(isAlertPresent());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	    	assertEquals("elephant 35 3", driver.findElement(By.className("elephant")).getText());
+		    } catch (Error e) {
+		      verificationErrors.append(e.toString());
+		    }
+	    
+	    try {
+	      assertEquals("$105.00", driver.findElement(By.id("subtotal")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	    	/* Nebraska Tax @ 5.00% */
+	      assertEquals("$5.25", driver.findElement(By.id("taxes")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	    try {
+	      assertEquals("$110.25", driver.findElement(By.id("total")).getText());
+	    } catch (Error e) {
+	      verificationErrors.append(e.toString());
+	    }
+	   
+	  }
   
   @After
   public void tearDown() throws Exception {
